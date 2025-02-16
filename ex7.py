f6dbbc967ace99be4742c77d7ab5b552f8b238c4
moab01567 <moab015@student.kristiania.no>
2025-02-16 21:48:51 +0100

    init

---
+++ ex7.py
@@ -0,0 +1,25 @@
+#priner Marry had a little lamb. på terminalen.  
+print("Marry had a little lamb.")
+#printer its fleece was white as på terminalev. og ved hjelp av format() funksjonen kan vi skrive inn stingen i den tomme linja.
+print("its fleece was white as {}".format("snow"))
+#printer and everything that mary want. på terminalen  
+print("and everything that mary want.")
+#printer den "." ti ganger på rad etter hverandre. 
+print("." * 10)# what'd that do?
+#gir hver varibael en string med en bokstav 
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch end = ' ' at end. try removing it to see what happens.
+print(end1 + end2 + end3 + end4 + end5 + end6+ , end= "")
+print(end7 + end8 + end9 + end10 + end11 + end12)
